bc8ea95b28e819a26bbd3c9bd665c812037d61e6
Danish Khan <dahmedk1999@gmail.com>
2021-11-21 12:25:02 -0800

    comments

--- HW3_2_Sniff_Spoof/Labsetup/volumes/sniffer.py
+++ HW3_2_Sniff_Spoof/Labsetup/volumes/sniffer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-ifaceC=['br-61846b5b5ea4', 'enp0s3']
+ifaceC=['br-61846b5b5ea4', 'enp0s3'] # Machine specific, find using ifconfig
+
 def print_pkt(pkt):
     pkt.show()
 print("Starting Packet Sniffing")
-pkt = sniff(iface=ifaceC, filter="dst net 128.230.0.0/16", prn=print_pkt,count=5)
+# pkt = sniff(iface=ifaceC, filter='icmp', prn=print_pkt) # ICMP Filter
+# pkt = sniff(iface=ifaceC, filter='tcp host 10.9.0.5 and port 23', prn=print_pkt) # TCP Host and Port filter
+pkt = sniff(iface=ifaceC, filter="dst net 128.230.0.0/16", prn=print_pkt,count=5) # Subnet Filter
